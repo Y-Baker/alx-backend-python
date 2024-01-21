e4f354ae445c03180e00cdf47e452c339b7d4530
Yousef <yuossefbakier@gmail.com>
2024-01-21 19:16:21 +0200

    Try Fix 101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,7 +8,7 @@ T = TypeVar('T')
 
 def safely_get_value(dct: Mapping, key: Any,
                      default: Optional[Union[T, None]] = None
-                     ) -> Union[T, Any]:
+                     ) -> Union[Any, T]:
     if key in dct:
         return dct[key]
     else:
