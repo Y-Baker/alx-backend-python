2b0ff9eb7a999906e35e90990ffab3ccd659f1ad
Yousef <yuossefbakier@gmail.com>
2024-01-21 18:14:35 +0200

    Add Task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" Let's duck type an iterable object"""
-from typing import Mapping, MutableMapping, Sequence, Iterable, List, Tuple
+"""Type Checking"""
+
+from typing import List, Tuple, Sequence, Iterable
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """ Element length """
     return [(i, len(i)) for i in lst]
