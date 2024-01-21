04e484b47a38b8b6b7e9a51dc806fb3e24830ede
Yousef <yuossefbakier@gmail.com>
2024-01-21 18:49:59 +0200

    Fix Code Style

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,7 +5,9 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """type-annotated function safe_first_element that takes a list input_list"""
+    """
+    type-annotated function safe_first_element that takes a list input_list
+    """
     if lst:
         return lst[0]
     else:
