9d8ec18e6e0d5b32f82cdd6d8eb4226c3a072584
Yousef <yuossefbakier@gmail.com>
2024-01-21 16:57:36 +0200

    Fix Code Style

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,6 +2,7 @@
 
 """Module that contains a type-annotated function"""
 
+
 def add(a: float, b: float) -> float:
     """Function that takes two floats and returns their sum as a float"""
     return a + b
