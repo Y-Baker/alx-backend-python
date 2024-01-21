a93a446ca5bc480aaa8d1df4948b2baeb74abca9
Yousef <yuossefbakier@gmail.com>
2024-01-21 17:08:36 +0200

    Add Task 3

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-""" Basic annotations - to string  """
+"""Module that contains a type-annotated function"""
 
 
 def to_str(n: float) -> str:
-    """ Returns the string representation of the float. """
+    """Function that takes a float n and returns the string representation of
+    the float"""
     return str(n)
