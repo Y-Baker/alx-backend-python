3738cde4232d7ce7503d6f1e5901243bb1d5613c
Yousef <yuossefbakier@gmail.com>
2024-01-22 21:33:18 +0200

    Add Task 2

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module for measure_time func"""
+
+import time, asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """measure time needed for wait_n func to complete"""
+    start: float = time.monotonic()
+    asyncio.run(wait_n(n, max_delay))
+
+    return (time.monotonic() - start) / n
