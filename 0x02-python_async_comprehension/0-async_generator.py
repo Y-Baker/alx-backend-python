d130e6e801a15e67e7d79ead497a71378b9b4118
Yousef <yuossefbakier@gmail.com>
2024-01-24 12:45:08 +0200

    Fix annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import random
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator:
+async def async_generator() -> Generator[float, None, None]:
     """will loop 10 times, each time asynchronously wait 1 second"""
     for i in range(10):
         await asyncio.sleep(1)
