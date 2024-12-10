e52726c01f3476e098fb3793693483b7dbd286fc
Yousef <yuossefbakier@gmail.com>
2024-12-10 15:02:55 +0200

    Add Time to logging

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -2,15 +2,20 @@
 
 import sqlite3
 import functools
+from datetime import datetime
 
 def log_queries(func):
+    @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        print(f"Query: {kwargs['query']}")
+        query = kwargs.get('query') or args[0]
+        timeNow = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"At {timeNow} - Query: {query} running...")
         return func(*args, **kwargs)
     return wrapper
 
 @log_queries
 def fetch_all_users(query):
+    """Fetch all users from the database"""
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
     cursor.execute(query)
