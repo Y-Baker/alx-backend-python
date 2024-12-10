687c1800bb12770d5254d46719e3c8d5aa6cca64
Yousef <yuossefbakier@gmail.com>
2024-12-10 13:48:40 +0200

    Fix To Checker

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -8,7 +8,7 @@ seed = __import__('seed')
 def paginate_users(page_size, offset):
     connection = seed.connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
-    cursor.execute(f"SELECT * FROM users ORDER BY user_id LIMIT {page_size} OFFSET {offset}")
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
     rows = cursor.fetchall()
     connection.close()
     return rows
