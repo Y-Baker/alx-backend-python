ef9b7231475517619fe8fdd4b8f61a6aecc75bbc
Yousef <yuossefbakier@gmail.com>
2024-12-10 15:42:10 +0200

    Task 2

---
+++ python-decorators-0x01/1-with_db_connection.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+create a decorator that automatically handles opening and closing database connections
+"""
+
+import sqlite3 
+import functools
+
+connector = __import__('seed').connect_db
+
+def with_db_connection(func):
+    """opens a database connection, passes it to the function and closes it afterword""" 
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = kwargs.get('conn') or (args[0] if len(args) > 0 else None)
+        user_id = kwargs.get('user_id') or (args[1] if len(args) > 1 else None)
+        if not conn:
+            conn = connector()
+        re = func(conn, user_id)
+        conn.close()
+        return re
+    return wrapper
+
+
+
+@with_db_connection 
+def get_user_by_id(conn, user_id): 
+    cursor = conn.cursor() 
+    cursor.execute("SELECT * FROM users WHERE user_id = ?", (user_id,)) 
+    return cursor.fetchone() 
+
+#### Fetch user by ID with automatic connection handling 
+user = get_user_by_id(user_id="70cef897-5095-44dd-ba79-a9efbf1f82a5")
+print(user)
